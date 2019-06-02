4ffb806f84be6fe62f5659815594baa210db5031
Diraksam <dmunajat@ucsd.edu>
2019-06-01 20:10:22 -0700

    This Version has the new registration and Login Screen.
    TODO:
    ---------------------------------
    MainScreen && Registration Screen
    ---------------------------------
    Connected to FB database, might need to add Name Field into the databse for better personalized message.
    
    Next ToDO:
    ----------------
    HomeScreen && BottomNavigation
    ----------------
    Need to integrate simons navigation bar logic. Then add my recycleView for images.
    Need to pull from database

--- app/src/main/java/com/example/whatssizzlin/HomeActivity.java
+++ app/src/main/java/com/example/whatssizzlin/HomeActivity.java
@@ -1,75 +1,67 @@
 package com.example.whatssizzlin;
 /*---------------------------Imports-------------------------------*/
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
 /*---------------------------Imports-------------------------------*/
 
-public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-    private BottomNavigationView mBtmView;
-    private int mMenuId;
+public class HomeActivity extends AppCompatActivity {
+
+    private final static String TAG = "HomeActivity: Imaging";
+    /*For our images into our view*/
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    /*For our images into our view*/
+
     /*OnCreate method*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        mBtmView = (BottomNavigationView) findViewById(R.id.BottomNavigation);
-        mBtmView.setOnNavigationItemSelectedListener(this);
-        mBtmView.getMenu().findItem(R.id.NavigationHome).setChecked(true);
+
+        getImages();
     }
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // uncheck the other items.
-        mMenuId = item.getItemId();
-        for (int i = 0; i < mBtmView.getMenu().size(); i++) {
-            MenuItem menuItem = mBtmView.getMenu().getItem(i);
-            boolean isChecked = menuItem.getItemId() == item.getItemId();
-            menuItem.setChecked(isChecked);
-        }
-        switch (item.getItemId()) {
-            case R.id.NavigationHome:
-                return true;
-            case R.id.NavigationSearch:
-                startActivity(new Intent(HomeActivity.this, SearchActivity.class));
-                return true;
-            case R.id.NavigationPantry:
-                startActivity(new Intent(HomeActivity.this, PantryActivity.class));
-                return true;
-            case R.id.NavigationPreferences:
-                startActivity(new Intent(HomeActivity.this, PreferencesActivity.class));
-                return true;
-            default:
-                return onNavigationItemSelected(item);
-        }
+
+    private void getImages(){
+        Log.d(TAG, "Inside getImages: ");
+
+        /* Need a way to pull image urls and image_names
+         * mImageUrls.add(Image URL stuff)
+         * mNames.add(Image URL stuff)
+         * */
+        mImageUrls.add("https://www.onceuponachef.com/images/2017/10/How-To-Make-Hard-Boiled-Eggs-760x516.jpg");
+        mNames.add("Eggs");
+
+        mImageUrls.add("https://www.thespruceeats.com/thmb/CtGnnAdHCVd5jms3JyTDfIgDzR0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/French-Toast-58addf8e5f9b58a3c9d41348.jpg");
+        mNames.add("Toast");
+        //mTimes.add()
+
+        mImageUrls.add("https://i.kinja-img.com/gawker-media/image/upload/s--_iU5hnjV--/c_scale,f_auto,fl_progressive,q_80,w_800/naiqjp2jbpvcylp09utj.png");
+        mNames.add("Batman");
+
+        mImageUrls.add("https://media.wired.com/photos/5c54ee6a4feec32ca0f590d8/master/w_2400,c_limit/superman-922909434.jpg");
+        mNames.add("Superman");
+
+
+        initRecyclerView();
     }
-//    @Override
-////    public boolean onCreateOptionsMenu(Menu menu) {
-////        MenuInflater inflater = getMenuInflater();
-////        inflater.inflate(R.menu.navigation, menu);
-////        return true;
-////    }
-////
-////    @Override
-////    public boolean onOptionsItemSelected(MenuItem item) {
-////        // Handle item selection
-////        switch (item.getItemId()) {
-////            case R.id.NavigationHome:
-////                return true;
-////            case R.id.NavigationSearch:
-////                startActivity(new Intent(HomeActivity.this, SearchActivity.class));
-////                return true;
-////            case R.id.NavigationPantry:
-////                startActivity(new Intent(HomeActivity.this, PantryActivity.class));
-////                return true;
-////            case R.id.NavigationPreferences:
-////                startActivity(new Intent(HomeActivity.this, PreferencesActivity.class));
-////                return true;
-////            default:
-////                return super.onOptionsItemSelected(item);
-////        }
-////    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "Initializing RecyclerView");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.recycleView);
+
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames, mImageUrls, this);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
 }
--- app/src/main/java/com/example/whatssizzlin/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/whatssizzlin/RecyclerViewAdapter.java
@@ -1,28 +1,84 @@
-//package com.example.whatssizzlin;
-//
-//
-//
-////viewHolderClass inside adapter
-//
-//import android.support.v7.widget.RecyclerView;
-//import android.view.View;
-//import android.widget.TextView;
-//
-//import de.hdodenhof.circleimageview.CircleImageView;
-//
-//public class RecyclerViewAdapter {
-//
-//    public class ViewHodler extends RecyclerView.ViewHolder{
-//
-//        CircleImageView image;
-//        TextView image_name;
-//
-//        //default constructor
-//
-//        public ViewHodler(View itemView) {
-//            super(itemView);
-//            image = itemView.findViewById(R.id.image_view);
-//        }
-//    }
-//
-//}
+package com.example.whatssizzlin;
+
+
+
+//viewHolderClass inside adapter
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+    //
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> names, ArrayList<String> imageUrls, Context context) {
+        this.mContext = context;
+        this.mNames = names;
+        this.mImageUrls = imageUrls;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_list_view, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImageUrls.get(position))
+                .into(holder.image);
+
+        holder.image_name.setText(mNames.get(position));
+
+        holder.image.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(TAG, "onClick: clicked on an image: " + mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNames.size(); //or mImageUrls since they will be the same
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView image_name;
+
+        //default constructor
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_view);
+            image_name = itemView.findViewById(R.id.name);
+        }
+    }
+
+}
