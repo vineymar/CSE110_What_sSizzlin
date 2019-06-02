200a6ed976a54af54cca36c2d32e9c7a04c832e6
Diraksam <dmunajat@ucsd.edu>
2019-06-02 16:54:14 -0700

    This Version has the new registration and Login Screen.
    TODO:
    ---------------------------------
    MainScreen && Registration Screen
    ---------------------------------
    Pantry is almost done. Need to UI
    ----------------
    HomeScreen && BottomNavigation
    ----------------
    TOP PRIORITY:
     - Need to integrate simons navigation bar logic.
     - Scrape database for ImageURL, RecipeName, Time
     - ViewRecipe Screen
     - Create/add/delete Screen
     - Pantry Screen

--- app/src/main/java/com/example/whatssizzlin/PantryFragment.java
+++ app/src/main/java/com/example/whatssizzlin/PantryFragment.java
@@ -7,7 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -26,45 +30,35 @@ public class PantryFragment extends Fragment  {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_pantry2, container, false);
 
-        String[] ingredientItemList = {
-                        "watermelon",
-                        "Bananas",
-                        "Pickles" };
+        final ArrayList<String> ingredientItemList = new ArrayList<String>();
+//            ingredientItemList.add("watermelon");
+//            ingredientItemList.add("Pickles");
+//            ingredientItemList.add("Bananas");
+
 
         ListView mIngredientList = view.findViewById(R.id.ingredientListView);
 
-        ArrayAdapter<String> mListViewAdapter = new ArrayAdapter<String>(
+        final ArrayAdapter<String> mListViewAdapter = new ArrayAdapter<String>(
                 getContext(),
                 android.R.layout.simple_list_item_1,
+                //R.id.listIngredientView,
                 ingredientItemList
         );
 
         mIngredientList.setAdapter(mListViewAdapter);
         mIngredientList.getAdapter();
 
-//        editTxt = (EditText) rootView.findViewById(R.id.ingredient_id);
-//        btnAddIngredient = (Button) rootView.findViewById(R.id.btnAdd);
-//        list = (ListView) rootView.findViewById(R.id.ingredientList);
-//        btnAddIngredient.setOnClickListener(this);
-//        arrayList = new ArrayList<String>();
-//
-//        // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
-//        // and the array that contains the data
-//        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
-//
-//        // Here, you set the data in your ListView
-//        list.setAdapter(adapter);
+        final EditText editText = view.findViewById(R.id.ingredient_id);
+        Button btnAddIngredient = view.findViewById(R.id.btnAdd);
+        btnAddIngredient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ingredientItemList.add(editText.getText().toString());
+                mListViewAdapter.notifyDataSetChanged();
+            }
+        });
 
         return view;
     }
 
-
-
-//    @Override
-//    public void onClick(View v) {
-//// this line adds the data of your EditText and puts in your array
-//        arrayList.add(editTxt.getText().toString());
-//        // next thing you have to do is check if your adapter has changed
-//        adapter.notifyDataSetChanged();
-//    }
 }
